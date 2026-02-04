9c544f31cd2ca623215e613e812a3c3404d59328
NikolaiUstimov <profildh@gmail.com>
2026-02-04 18:44:15 +0300

    Add diagramm

--- src/app/features/repair-list/repair-list.component.ts
+++ src/app/features/repair-list/repair-list.component.ts
@@ -1,13 +1,18 @@
-import {Component, inject} from '@angular/core';
-import {Field} from '../../shared/field/field';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {ButtonComponent} from '../../shared/button/button.component';
-import {DateUtils} from '../../core/utils/date-utils';
-import {RepairType} from '../../types/repair-type';
-import {SvgIconComponent} from '../../shared/svg-icon/svg-icon.component';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  inject
+} from '@angular/core'
+import {Field} from '../../shared/field/field'
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {ButtonComponent} from '../../shared/button/button.component'
+import {DateUtils} from '../../core/utils/date-utils'
+import {RepairType} from '../../types/repair-type'
+import {SvgIconComponent} from '../../shared/svg-icon/svg-icon.component'
 import {
   StatisticRepairService
-} from '../../core/services/statistic-repair.service';
+} from '../../core/services/statistic-repair.service'
 
 @Component({
   selector: 'app-repair-list',
@@ -19,11 +24,17 @@ import {
     SvgIconComponent
   ],
   templateUrl: './repair-list.component.html',
-  styleUrl: './repair-list.component.scss'
+  styleUrl: './repair-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RepairListComponent {
+  cdr = inject(ChangeDetectorRef);
   #fb = inject(FormBuilder);
   statisticRepair = inject(StatisticRepairService);
+
+  constructor() {
+    this.cdr.markForCheck()
+  }
 
   repairForm = this.#fb.group({
     nameRepair: ['', Validators.required],
@@ -34,6 +45,7 @@ export class RepairListComponent {
   addNewRepair() {
     const currentDate = new Date();
     const listData: RepairType[] = this.statisticRepair.dataRepair();
+
 
     const nextNumber: number = listData.length
       ? listData[listData.length - 1].number + 1
---
+++ src/app/shared/diagramm/diagramm.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagramm',
+  imports: [],
+  templateUrl: './diagramm.component.html',
+  styleUrl: './diagramm.component.scss'
+})
+export class DiagrammComponent {
+
+}
